7f2a206fdf813e21800d5875f47c078395ec7b60
totheralistair <totheralistair@aol.com>
2014-04-13 19:12:29 -0400

    tests work from rackup, MockRequest, Sinatra

---
+++ src/handle_get.rb
@@ -0,0 +1,9 @@
+# send all HTTP GET requests here
+require 'rack'
+
+def handle_get( req ) # expect Rack::Request, return Rack::Response
+
+end
+
+
+
---
+++ src/handle_thru_sinatra.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require_relative 'handle_get'
+
+get '*' do
+  response = handle_get(request);
+  out = response[2].body
+end
+post '/post' do ml_add(request) ;end
+
+
