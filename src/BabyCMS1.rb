3b0b2f3cae54cec9be9e444b1b169a3927ed0326
totheralistair <totheralistair@aol.com>
2014-04-23 15:06:05 -0400

    w & w/o server, GET and POST
    hopefully starting to look right

--- src/BabyCMS1.rb
+++ src/BabyCMS1.rb
@@ -11,22 +11,24 @@ class BabyCMS
   end
 end
 
-def handle_get( req )
-puts "in handle get"
+def handle_get( request )
+  params = request.params
   response = Rack::Response.new
   response['Content-Type'] = 'text/html'
-  response.write "Nice GET there. From Alistair"
+  response.write "Nice GET there. "
+  response.write "params = #{params}."
+  response.write " Bests. Alistair."
   response.finish
 end
 
 # send all HTTP POST requests here
 
-def handle_post( req ) # expect Rack::Request, return Rack::Response
-puts "in handle post"
-puts "req"
+def handle_post( request ) # expect Rack::Request, return Rack::Response
+  params = request.params
   response = Rack::Response.new
   response['Content-Type'] = 'text/html'
-  response.write "Got that POST, baby."
+  response.write "Got that POST, baby. "
+  response.write "params = #{params}."
   response.finish
 end
 
--- test/test_babyCMS1.rb
+++ test/test_babyCMS1.rb
@@ -6,29 +6,35 @@ require_relative '../src/BabyCMS1.rb'
 class TestRequestsToBabyCMS < Test::Unit::TestCase
   include Rack::Test::Methods
 
-  def get_without_server(app, method, route)    # parameterized for GETs and POSTs
-    Rack::MockRequest.new(app).request(method, route)
+  def run_without_server(app, method, route, params={})    # parameterized for GETs and POSTs
+    aRequest = Rack::MockRequest.new(app)
+    aRequest.request(method, route, {:params=>params})
   end
 
-  def get_with_server(app, method, route)      # still only GETs
-    last_response = Rack::Test::Session.new(app).get "/"
+  def run_with_server(app, method, route, params={})      # still only GETs
+        aSession = Rack::Test::Session.new(app)
+        aSession.request route, {:method=>method}.merge(:params=>params)
   end
+
+# p.s. I don't understand the difference above between MockRequest and Session
 
 #=================================================
     def test_00_get_without_server
-      puts "get without server"
       app = BabyCMS.new
-      last_response = get_without_server( app, "GET", '/' )
-      expectedResult = "Nice GET there. From Alistair"
-      last_response.body.should == expectedResult
+      run_without_server( app, "GET", '/blarg', "getKey=getValue").body.should ==
+          "Nice GET there. params = {\"getKey\"=>\"getValue\"}. Bests. Alistair."
+      run_without_server( app, "POST", '/ook', "postKey=postValue" ).body.should ==
+          "Got that POST, baby. params = {\"postKey\"=>\"postValue\"}."
     end
 
-    def test_01_get_with_server
-      puts "get using server"
+    def test_01_RUN_with_server
       app = BabyCMS.new
-      last_response = get_with_server( app, "GET", '/')
-      expectedResult = "Nice GET there. From Alistair"
-      last_response.body.should == expectedResult
+      run_with_server( app, "GET", '/sweetie', "getKey=getValue").body.should ==
+          "Nice GET there. params = {\"getKey\"=>\"getValue\"}. Bests. Alistair."
+      run_with_server( app, "POST", '/narf', "postKey=postValue").body.should ==
+          "Got that POST, baby. params = {\"postKey\"=>\"postValue\"}."
+      # don't run this next test except to test that it blows up.
+      #run_with_server( app, "RUBBISH", '/').body.should == "should just blow up."
     end
 
 end
