412f940b65fb6b8d5bbc6971d8c978acc809a8c9
totheralistair <totheralistair@aol.com>
2014-04-23 11:58:42 -0400

    refactored, simplified
    refactored, simplified, that's all. trying to see if Rack::Test::Session
    really uses a server; simplification is good, not using a server isn't.
    Saw Rack::Test::Methods, which looks like it might do all this, but
    committing this first.

--- test/test_babyCMS1.rb
+++ test/test_babyCMS1.rb
@@ -1,33 +1,34 @@
 require 'rspec/expectations'
 require 'test/unit'
 require 'rack/test'
+require_relative '../src/BabyCMS1.rb'
 
 class TestRequestsToBabyCMS < Test::Unit::TestCase
   include Rack::Test::Methods
 
-  def get_without_server(method, route, expectedResult)    # parameterized for GETs and POSTs
-    require_relative '../src/BabyCMS1.rb'
-    app = BabyCMS.new
-    last_response = Rack::MockRequest.new(app).request(method, route)
-    last_response.body.should == expectedResult
+  def get_without_server(app, method, route)    # parameterized for GETs and POSTs
+    Rack::MockRequest.new(app).request(method, route)
   end
 
-  def get_with_server(method, route, expectedResult)      # still only GETs
-    app = BabyCMS.new
-    session = Rack::Test::Session.new(app)
-    last_response = session.get "/"
-    last_response.body.should == expectedResult
+  def get_with_server(app, method, route)      # still only GETs
+    last_response = Rack::Test::Session.new(app).get "/"
   end
 
 #=================================================
     def test_00_get_without_server
       puts "get without server"
-      get_without_server("GET", '/', "Nice GET there. From Alistair")
+      app = BabyCMS.new
+      last_response = get_without_server( app, "GET", '/' )
+      expectedResult = "Nice GET there. From Alistair"
+      last_response.body.should == expectedResult
     end
 
     def test_01_get_with_server
       puts "get using server"
-      get_with_server("GET", '/', "Nice GET there. From Alistair")
+      app = BabyCMS.new
+      last_response = get_with_server( app, "GET", '/')
+      expectedResult = "Nice GET there. From Alistair"
+      last_response.body.should == expectedResult
     end
 
 end
